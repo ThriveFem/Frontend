d6b1c5906318d1361001f642e337e6deedd6cccc
yarbework <yarbemohaz3517@gmail.com>
2026-06-06 12:12:54 +0300

    setting up folder structure

---
+++ ThriveFem/theme/colors.js
@@ -0,0 +1,8 @@
+export const COLORS = {
+  background: '#001B3A', // Base frame fill
+  primary: '#FF69B4',    // Structural highlight 1
+  secondary: '#F9A8D4',  // Structural highlight 2
+  text: '#FDFCF0',       // Core font data
+  card: '#0A2647',       // Slightly lighter blue for cards
+  statusGreen: '#4ADE80' // For the "Wearable Simulator Online" flag
+};
